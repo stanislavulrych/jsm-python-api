f4dbd5a974b3641e7dbc346b642d8a90c7617e29
Stanislav Ulrych <stanislav.ulrych@gmail.com>
2024-09-08 21:53:42 +0200

    fix: simplified requests handling

--- jiraservicemanagement/api/v1.py
+++ jiraservicemanagement/api/v1.py
@@ -6,59 +6,56 @@ class Assets(AtlassianRestAPI):
     """
 
     def __init__(self, workspaceId, auth):
-        self.workspaceId = workspaceId
-        super().__init__(**auth)
+        args = { **auth, **{ "url": auth['url'] + f"gateway/api/jsm/assets/workspace/{workspaceId}/v1" } }
+        super().__init__(**args)
 
-    def _workspaceUrl(self):
-        return f"gateway/api/jsm/assets/workspace/{self.workspaceId}/v1"
-    
     def get_object(self, objectId):
-        return self.get(self._workspaceUrl() + f"/object/{objectId}", headers=self.experimental_headers)
+        return self.get(f"/object/{objectId}", headers=self.experimental_headers)
 
     def get_object_attributes(self, objectId):
-        return self.get(self._workspaceUrl() + f"/object/{objectId}/attributes", headers=self.experimental_headers)
+        return self.get(f"/object/{objectId}/attributes", headers=self.experimental_headers)
 
     def get_object_history(self, objectId):
-        return self.get(self._workspaceUrl() + f"/object/{objectId}/history", headers=self.experimental_headers)
+        return self.get(f"/object/{objectId}/history", headers=self.experimental_headers)
 
     def get_object_reference_info(self, objectId):
-        return self.get(self._workspaceUrl() + f"/object/{objectId}/referenceinfo", headers=self.experimental_headers)
+        return self.get(f"/object/{objectId}/referenceinfo", headers=self.experimental_headers)
 
     def get_object_connected_tickets(self, objectId):
-        return self.get(self._workspaceUrl() + f"/objectconnectedtickets/{objectId}/tickets", headers=self.experimental_headers)
+        return self.get(f"/objectconnectedtickets/{objectId}/tickets", headers=self.experimental_headers)
 
     def post_object_aql(self, query):
-        return self.post(self._workspaceUrl() + "/object/aql", json={ "qlQuery": query }, headers=self.experimental_headers)
+        return self.post("/object/aql", json={ "qlQuery": query }, headers=self.experimental_headers)
     
     def post_object_navlist_aql(self, query):
-        return self.post(self._workspaceUrl() + "/object/navlist/aql", json={ "qlQuery": query }, headers=self.experimental_headers)
+        return self.post("/object/navlist/aql", json={ "qlQuery": query }, headers=self.experimental_headers)
     
     def list_object_schema(self):
-        return self.get(self._workspaceUrl() + "/objectschema/list", headers=self.experimental_headers)
+        return self.get("/objectschema/list", headers=self.experimental_headers)
 
     def get_object_schema(self, objectSchemaId):
-        return self.get(self._workspaceUrl() + f"/objectschema/{objectSchemaId}", headers=self.experimental_headers)
+        return self.get(f"/objectschema/{objectSchemaId}", headers=self.experimental_headers)
 
     def get_object_schema_attributes(self, objectSchemaId):
-        return self.get(self._workspaceUrl() + f"/objectschema/{objectSchemaId}/attributes", headers=self.experimental_headers)
+        return self.get(f"/objectschema/{objectSchemaId}/attributes", headers=self.experimental_headers)
 
     def get_object_schema_objecttypes(self, objectSchemaId, flat=False):
-        url = self._workspaceUrl() + f"/objectschema/{objectSchemaId}/objecttypes"
+        url = f"/objectschema/{objectSchemaId}/objecttypes"
         if flat:
             url += "/flat"
         return self.get(url, headers=self.experimental_headers)
         
     def get_object_type(self, objectTypeId):
-        return self.get(self._workspaceUrl() + f"/objecttype/{objectTypeId}", headers=self.experimental_headers)
+        return self.get(f"/objecttype/{objectTypeId}", headers=self.experimental_headers)
     
 
     def get_object_type_attributes(self, objectTypeId):
-        return self.get(self._workspaceUrl() + f"/objecttype/{objectTypeId}/attributes", headers=self.experimental_headers)
+        return self.get(f"/objecttype/{objectTypeId}/attributes", headers=self.experimental_headers)
 
     def list_config_status_type(self):
-        return self.get(self._workspaceUrl() + "/config/statustype", headers=self.experimental_headers)
+        return self.get("/config/statustype", headers=self.experimental_headers)
     
     def get_config_status_type(self, statusTypeId):
-        return self.get(self._workspaceUrl() + f"/config/statustype/{statusTypeId}", headers=self.experimental_headers)
+        return self.get(f"/config/statustype/{statusTypeId}", headers=self.experimental_headers)
     
 
